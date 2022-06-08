6dd6b3f2245799d58a0886b4e7f453526a6910e7
Lorenzo Tomas Diez <lorenzotomasdiez@MacBook-Air-de-Lorenzo.local>
2022-06-08 00:12:50 -0300

    functions 2

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -39,8 +39,22 @@
 
     }
 
-    
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        if ( isDead ) return 1500;
+        if ( isSeparated ) return 2500
+        return isRetired ? 3000 : 4000;
+    }
 
+    /* const getPayAmountOld = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        if ( isDead ) {
+            return 1500;
+        } 
+        if ( isSeparated ) {
+            return 2500;
+        } 
+        
+        return isRetired ? 3000 : 4000;
+    } */
 
 })();
 
