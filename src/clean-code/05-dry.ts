6669bbc436d32b1adba70a01052ab10467b54683
Lorenzo Tomas Diez <lorenzotomasdiez@MacBook-Air-de-Lorenzo.local>
2022-06-08 22:07:36 -0300

    dry principles

---
+++ src/clean-code/05-dry.ts
@@ -0,0 +1,36 @@
+type Size = '' | 'S' | 'M' | 'XL'
+class Product{
+    constructor(
+        public name:string = '',
+        public price: number = 0,
+        public inStock: boolean = false,
+        public size: Size = ''
+    ){}
+
+    isProductReady():boolean{
+        for (const key in this) {
+            switch(typeof this[key]){
+                case 'string':
+                    if((<string><unknown>this[key]).length <= 0)throw Error(`${key} is empty`);
+                    break;
+                case 'number':
+                    if((<number><unknown>this[key]) <= 0)throw Error(`${key} is empty`);
+                    break;
+                default:
+                    throw Error(`${typeof this[key]} is not supported`);
+            }
+        }
+
+        return true;
+    }
+
+    toString(){
+        if(!this.isProductReady) return;
+        return `${this.name} (${this.price}), ${this.size}`
+    }
+}
+
+(()=>{
+    const bluePants = new Product('Blue Large Pants');
+    console.log(bluePants.toString());
+})();
